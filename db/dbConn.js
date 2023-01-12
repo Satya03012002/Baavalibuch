0aec7f312d361a6829ad41c45243ebed8c055a25
My Name <my-name@chromium.org>
2023-01-12 12:39:32 +0530

    k

--- db/dbConn.js
+++ db/dbConn.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-const CONNECTION_URL = "mongodb+srv://Baavalibuch:Baavalibuch@cluster0.ddmrzxa.mongodb.net/test"
+import dotenv from "dotenv";
+dotenv.config({ silent: process.env.NODE_ENV === 'production' });
+const CONNECTION_URL = `mongodb+srv://${process.env.DBNAME}:${process.env.PASSWORD}@cluster0.ddmrzxa.mongodb.net/test`
 mongoose.set("strictQuery", false);
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log("mongodb connected successfully"))
--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import "./db/dbConn.js";
 import cors from 'cors';
-
 import SignInRouter from "./router/loginRequest/router.js";
+import dotenv from "dotenv";
+dotenv.config({ silent: process.env.NODE_ENV === 'production' });
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
